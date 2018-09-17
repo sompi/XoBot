0fe4ad7161ff97c7a46d524511a9638473b49898
UhWatchMe <ibeari@live.nl>
2018-09-17 16:28:40 +0200

    Updated to newest API + removed stuff.

--- Magic/nonPlankMaker/src/nonPlankMaker.java
+++ Magic/nonPlankMaker/src/nonPlankMaker.java
@@ -5,15 +5,12 @@ import java.util.Locale;
 
 import xobot.client.callback.listeners.MessageListener;
 import xobot.client.callback.listeners.PaintListener;
+import xobot.client.events.MessageEvent;
 import xobot.script.ActiveScript;
 import xobot.script.Manifest;
 import xobot.script.methods.Bank;
-import xobot.script.methods.Game;
 import xobot.script.methods.GameObjects;
 import xobot.script.methods.Packets;
-import xobot.script.methods.Players;
-import xobot.script.methods.input.KeyBoard;
-import xobot.script.methods.tabs.Equipment;
 import xobot.script.methods.tabs.Inventory;
 import xobot.script.methods.tabs.Skills;
 import xobot.script.util.Time;
@@ -24,21 +21,18 @@ import xobot.script.wrappers.interactive.Item;
 
 
 
-@Manifest(authors = { "NonCombat" }, name = "nonPlankMaker", version = 1.0, description = "Makes planks using lunar spell, start at home.")
+@Manifest(authors = { "NonCombat" }, name = "nonPlankMaker", version = 1.2, description = "Makes planks using lunar spell, start at home.")
 
 public final class nonPlankMaker extends ActiveScript implements PaintListener, MessageListener {
 
 	private Timer t;
-	String version = "1.1";
-	private int startxp;
-	private int startLvl;
+	String version = "1.2";
+	int startxp;
 	int oakLog = 1521;
 	int mahoganyLog = 6332;
 	int earthrune = 557;
 	int naturerune = 561;
 	int astralrune = 9075;
-	int earthstaff = 1385;
-	int plankMakeAnimation = 6298; //Stays active until none planks
 	int logs[] = {oakLog, mahoganyLog};
 	int planks[] = {8778, 8782};
 	int bankID = 26972;
@@ -50,28 +44,16 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 	
 	public boolean onStart() {
 		this.t = new Timer(System.currentTimeMillis());
-        this.startxp = Skills.getCurrentExp(Skills.MAGIC);
-        this.startLvl = Skills.getCurrentLevel(Skills.MAGIC);
+        startxp = Skills.MAGIC.getCurrentExp();
 		return true;
 	}
 	
 	public void onExit() {
-		System.out.println("Testing");
+
 	}
 	
 	public boolean hasLogs() {
 		return Inventory.Contains(logs);
-	}
-	
-	public int logAmount() {
-		int oakLogCount = Inventory.getCount(oakLog);
-		int mahoganyLogCount = Inventory.getCount(mahoganyLog);		
-		return oakLogCount + mahoganyLogCount;
-	}
-	
-	public int logAmountBank() {
-
-		return 1;
 	}
 	
 	public boolean hasNatureRunes() {
@@ -82,13 +64,20 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 		return Inventory.Contains(astralrune);
 	}
 	
-	public boolean hasEarthRunes() {
-		return Inventory.Contains(earthrune);
-	}
-	
 	public void makePlanks() {
 	    status = "Making planks.";
-		Packets.sendAction(315, 0, 0, 30029, 0, 0);
+		Packets.sendAction(315, 0, 0, 30029);
+		//Spelbook open
+		/*Mouse.HopMouse(745, 187);
+		Time.sleep(200);
+		Mouse.click(745, 187, true);
+		Time.sleep(200);
+
+		//Lunar spell plank make
+		Mouse.HopMouse(720, 333);
+		Time.sleep(200);
+		Mouse.click(720, 333, true);
+		Time.sleep(200);*/
 		while (doneMakingPlanks == false) {
 			//System.out.println("Making planks...");
 			Time.sleep(400);
@@ -97,7 +86,7 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
             }
 		}
 	}
-	
+
 	public int oakLogAmountBank() {
 		if (Bank.getItem(oakLog) != null) {
 			return Bank.getItem(oakLog).getStack();
@@ -105,7 +94,7 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 			return 0;
 		}
 	}
-	
+
 	public int mahoganyLogAmountBank() {
 		if (Bank.getItem(mahoganyLog) != null) {
 			return Bank.getItem(mahoganyLog).getStack();
@@ -113,32 +102,18 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 			return 0;
 		}
 	}
-	
+
 	public int natureRunesAmount() {
 		int bank = 0;
 		int inventory = 0;
 		if (Bank.getItem(naturerune) != null) {
 			bank = Bank.getItem(naturerune).getStack();
 		}
-		
+
 		if (Inventory.getItem(naturerune) != null) {
 			inventory = Inventory.getItem(naturerune).getStack();
 		}
-		
-		return bank + inventory;
-	}
-	
-	public int astralRunesAmount() {
-		int bank = 0;
-		int inventory = 0;
-		if (Bank.getItem(astralrune) != null) {
-			bank = Bank.getItem(astralrune).getStack();
-		}
-		
-		if (Inventory.getItem(astralrune) != null) {
-			inventory = Inventory.getItem(astralrune).getStack();
-		}
-		
+
 		return bank + inventory;
 	}
 	
@@ -153,9 +128,20 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 		
 		if (Time.sleep(()-> Bank.isOpen(), 7000)) {
 			System.out.println("Bank is open.");
+
+			/*if (Inventory.Contains(planks)){
+				System.out.println("Banking planks.");
+				for (int i = 0; i < planks.length; i++){
+					if (Inventory.getItem(planks[i]) != null){
+						Bank.withdraw(planks[i], 28);
+						Time.sleep(300);
+					}
+				}
+			}*/
+
 			if (Inventory.Contains(naturerune) && Inventory.Contains(astralrune)) {
 				if (Logs != null){
-					Packets.sendAction(431, Logs.getID(), Logs.getSlot(), 5064, 26972, 0);
+					Packets.sendAction(431, Logs.getID(), Logs.getSlot(), 5064);
 				}
 
 			//Bank.depositAllExcept(naturerune, astralrune);
@@ -179,13 +165,14 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 				Time.sleep(200);
 				}
 			}
-
 		}
 
-		Packets.sendAction(200, 0, 0, 5384, 0); // Packet to close bank
+		Packets.sendAction(200, 0, 0, 5384); // Packet to close bank
+		/*Mouse.HopMouse(488, 27);
+		Time.sleep(200);
+		Mouse.click(488, 27, true);
+		Time.sleep(200);*/
         Time.sleep(1000);
-        logAmount();
-        System.out.println("We have now have " + logAmount() + " in our inventory");
 	}
 
 	public int loop() {
@@ -199,58 +186,26 @@ public final class nonPlankMaker extends ActiveScript implements PaintListener, 
 			bank();
 		}
 		
-		if (oakLogAmountBank() < 1 && mahoganyLogAmountBank() < 1 && logAmount() < 1) {
-			System.out.println("Out of logs");
-			return -1;
-		}
-		
-		if (astralRunesAmount() <= 2 || natureRunesAmount() <= 1) {
-			System.out.println("Out of runes");
-			return -1;
-		}
-
-		if (outOfRunes == true){
-		    return -1;
-        }
-		
 		return 1000;
 	}
 
 	public void repaint(Graphics g1) {
 		
-		int xp = Skills.getCurrentExp(Skills.MAGIC) - this.startxp;
+		int xp = Skills.MAGIC.getCurrentExp() - this.startxp;
 		int xph = (int) (xp * 3600000.0D / this.t.getElapsed());
 		
 		g1.drawString("Time running " + this.t.toElapsedString(), 50, 85);
         g1.drawString("XP Gained: " + NumberFormat.getInstance(Locale.US).format(xp), 50, 100);
         g1.drawString("XP(h): " +  NumberFormat.getInstance(Locale.US).format(xph), 50, 115);
-        g1.drawString("Total logs in bank " + (oakLogAmountBank() + mahoganyLogAmountBank()), 50, 130);
-        g1.drawString("Status " + status, 50, 145);
-        g1.drawString("Version " + version, 50, 160);
+        g1.drawString("Status " + status, 50, 130);
+        g1.drawString("Version " + version, 50, 145);
 		
-	}
-	
-	public static float RoundItUp(float number, int decimalPlace) {
-		if (number > 0) {
-		BigDecimal bd = new BigDecimal(number);
-		bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
-		
-		return bd.floatValue();
-		} else {
-			return 0;
-		}
 	}
 
 	@Override
-	public void MessageRecieved(String msg, int arg1, String arg2) {
-		msg.toLowerCase();
-		if (msg.contains("You don't have anymore logs to turn into planks.")) {
+	public void MessageRecieved(MessageEvent messageEvent) {
+		if (messageEvent.getType() == 0 && messageEvent.getMessage().contains("You don't have anymore logs")){
 			doneMakingPlanks = true;
-			System.out.println("Not making planks anymore");
 		}
-
-		if (msg.contains("You don't have the required runes to cast this spell.")){
-		    outOfRunes = true;
-        }
 	}
 }
